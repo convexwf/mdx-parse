3a9519ee608a4df2797557f08f51e78a0defd10b
convexwf <convexwf@gmail.com>
2023-05-23 16:06:07 +0800

    add mdx reader class

---
+++ mdx_reader/__init__.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Project : mdx-parser
+# @FileName : __init__.py
+# @Author : convexwf@gmail.com
+# @CreateDate : 2023-05-23 16:06
+# @UpdateTime : 2023-05-23 16:06
+
+from .moji_reader import MojiReader
+
+__all__ = ["MojiReader"]
